58ac5dba6a0095dc01cd86b221fc319f2b703280
xex <u202110854@hust.edu.cn>
2023-08-05 01:00:08 +0800

    1. test

--- main.py
+++ main.py
@@ -114,23 +114,23 @@ def start_door():
 
             border = cv2.copyMakeBorder(handling, 12, 12, 16, 16, borderType=cv2.BORDER_CONSTANT,
                                         value=(255, 255, 255))  # 扩展白边，防止边界无法识别
-            handling = cv2.resize(border, (chest_width, chest_height), interpolation=cv2.INTER_CUBIC)  # 将图片缩放
+            handling = cv2.resize(border, (head_width, head_height), interpolation=cv2.INTER_CUBIC)  # 将图片缩放
             frame_gauss = cv2.GaussianBlur(handling, (21, 21), 0)  # 高斯模糊
             frame_hsv = cv2.cvtColor(frame_gauss, cv2.COLOR_BGR2HSV)  # 将图片转换到HSV空间
 
-            frame_hsv = frame_hsv[intercept:480, 0:640]     #  裁剪掉图像上半部分
+            frame_hsv = frame_hsv[0:480, 0:intercept]  # 裁剪掉图像右边缘部分
 
             frame_door_yellow = cv2.inRange(frame_hsv, start_door_color_range['yellow_door'][0],
-                                            start_door_color_range['yellow_door'][1])           # 对原图像和掩模(颜色的字典)进行位运算
+                                            start_door_color_range['yellow_door'][1])  # 对原图像和掩模(颜色的字典)进行位运算
             frame_door_black = cv2.inRange(frame_hsv, start_door_color_range['black_door'][0],
-                                           start_door_color_range['black_door'][1])             # 对原图像和掩模(颜色的字典)进行位运算
+                                           start_door_color_range['black_door'][1])  # 对原图像和掩模(颜色的字典)进行位运算
             frame_door = cv2.add(frame_door_yellow, frame_door_black)
 
             open_pic = cv2.morphologyEx(frame_door, cv2.MORPH_OPEN, np.ones((5, 5), np.uint8))  # 开运算 去噪点
             closed_pic = cv2.morphologyEx(open_pic, cv2.MORPH_CLOSE, np.ones((50, 50), np.uint8))  # 闭运算 封闭连接
             (contours, hierarchy) = cv2.findContours(closed_pic, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)  # 找出轮廓
             areaMaxContour, area_max = utils.getAreaMaxContour1(contours)  # 找出最大轮廓
-            percent = round(100 * area_max / (chest_width * chest_height), 2)  # 最大轮廓的百分比
+            percent = round(100 * area_max / (head_width * head_height), 2)  # 最大轮廓的百分比
 
             if Debug:
                 cv2.line(closed_pic, [0, intercept], [640, intercept], (100, 255, 100), 1)
